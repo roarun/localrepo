e565d6b65c5754d4421bd7ee55375d923d5765bc
aroon.paudel@gmail.com <135916160+roarun@users.noreply.github.com>
2023-09-16 14:18:03 -0700

    feature1 texts

--- new.c
+++ new.c
@@ -2,6 +2,6 @@
 int main(int argc, char const *argv[])
 {
     printf("Hello World!")
-    printf("This is feature1 branch")
+    printf("This is feature1 branch with a similar change that I am trying to make")
     return 0;
 }
