857982cdd9aebcdd0a0fb075dc4d6999f21d49ab
aroon.paudel@gmail.com <135916160+roarun@users.noreply.github.com>
2023-09-17 10:09:01 -0700

    stdio.h

--- new.c
+++ new.c
@@ -1,8 +1,6 @@
-#include 
+#include <stdio.h>
 int main(int argc, char const *argv[])
 {
     printf("Hello World!")
-    printf("This is feature1 branch with a similar change that I am trying to make")
-    printf("This is feature1 branch and I am trying to learn here!")
     return 0;
 }
